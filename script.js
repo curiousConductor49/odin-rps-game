114c08804196a2143e5c22dcbf93e945ec94cc77
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-29 13:18:15 -0400

    Refactor code for better efficiency

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ const displayDiv = document.querySelector(".display");
 const scoreReport = document.querySelector(".score-report");
 const roundReport = document.querySelector(".round-report");
 
-// Initialize variables for future storage of choice functions 
-let humanPick;
-let computerPick;
-
 // Initialize score variables
 let humanScore = 0;
 let computerScore = 0;
 let winningScore = 0;
 
 // Display results in DOM
-scoreReport.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`;
+scoreReport.textContent = `Your Score: ${humanScore} VS Computer Score: ${computerScore}`;
 roundReport.textContent = "The round winner is...";
 
 // Declare getComputerChoice function
@@ -24,21 +20,93 @@ function getComputerChoice () {
     // Initialize a variable to a random number between 1 and 3, inclusive
     const randomNum = Math.floor(Math.random() * 3) + 1;
 
-    // If random number is 1, 2, or 3, return string for rock, paper, or scissors
-    if (randomNum === 1) {
-        return "Rock";
-    } else if (randomNum === 2) {
-        return "Paper";
-    } else if (randomNum === 3) {
-        return "Scissors";
+    // Return string for rock, paper, or scissors based on random number value
+    switch (randomNum) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
     }
 }
 
 // Declare getHumanChoice function
 function getHumanChoice (event) {
-    // Obtain and return user choice between “rock”, “paper”, or “scissors”
-    const humanHandChoice = event.target.value;
-    return humanHandChoice;
+    // Return user choice between “rock”, “paper”, or “scissors”
+    return event.target.value;
+}
+
+// Declare playGame function
+function playGame(e) {
+    // Obtain human and computer choices
+    let humanPick = getHumanChoice(e);
+    let computerPick = getComputerChoice();
+    // Call playRound function
+    playRound(humanPick, computerPick);
+
+    // Display winner results
+    if (humanScore === 5) {
+        displayDiv.textContent = "You win the game!";
+    } else if (computerScore === 5) {
+        displayDiv.textContent = "Computer wins the game!";
+    }
+}
+
+// Declare playRound function to compare human and computer choices
+function playRound (humanChoice, computerChoice) {
+    // If block for human choice being rock
+    if (humanChoice === "Rock") {
+        // Switch statement for computer choice
+        switch (computerChoice) {
+            case "Rock":
+                roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
+                break;
+            case "Paper":
+                roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}!`;
+                ++computerScore;
+                break;
+            case "Scissors":
+                roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
+                ++humanScore;
+                break;
+        }
+    } 
+    // Else-if block for human choice being paper
+    else if (humanChoice === "Paper") {
+        switch (computerChoice) {
+            case "Rock":
+                roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+                ++humanScore;
+                break;
+            case "Paper":
+                roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
+                break;
+            case "Scissors":
+                roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}.`;
+                ++computerScore;
+                break;
+        }
+    }
+    // Else-if block for human choice being scissors
+    else if (humanChoice === "Scissors") {
+        switch (computerChoice) {
+            case "Rock":
+                roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}!`;
+                ++computerScore;
+                break;
+            case "Paper":
+                roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+                ++humanScore;
+                break;
+            case "Scissors":
+                roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
+                break;
+        }
+    }
+
+    // Update display div
+    scoreReport.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`;
 }
 
 // Add event listeners to player selection buttons
@@ -53,64 +121,3 @@ paperBtn.addEventListener("click", (e) => {
 scissorsBtn.addEventListener("click", (e) => {
     playGame(e);
 });
-
-function playGame(e) {
-    humanPick = getHumanChoice(e);
-    computerPick = getComputerChoice();
-    playRound(humanPick, computerPick);
-    if (humanScore === 5) {
-        displayDiv.textContent = "You win!";
-    } else if (computerScore === 5) {
-        displayDiv.textContent = "Computer wins!";
-    }
-}
-
-// Declare playRound function to compare human and computer choices
-function playRound (humanChoice, computerChoice) {
-    // Use nested if-else statements to compare choices
-    // If-else block for human choice being rock
-    if (humanChoice === "Rock") {
-        if (computerChoice === "Rock") {
-            roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
-        }
-        else if (computerChoice === "Paper") {
-            roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}!`;
-            ++computerScore;
-        }
-        else if (computerChoice === "Scissors") {
-            roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}!`;
-            ++humanScore;
-        }
-    } 
-    // Else-if block for human choice being paper
-    else if (humanChoice === "Paper") {
-        if (computerChoice === "Rock") {
-            roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
-            ++humanScore;
-        }
-        else if (computerChoice === "Paper") {
-            roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
-        }
-        else if (computerChoice === "Scissors") {
-            roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}.`;
-            ++computerScore;
-        }
-    }
-    // Else-if block for human choice being scissors
-    else if (humanChoice === "Scissors") {
-        if (computerChoice === "Rock") {
-            roundReport.textContent = `You lose! ${humanChoice} can't beat ${computerChoice}!`;
-            ++computerScore;
-        }
-        else if (computerChoice === "Paper") {
-            roundReport.textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
-            ++humanScore;
-        }
-        else if (computerChoice === "Scissors") {
-            roundReport.textContent = `${humanChoice} is the same as ${computerChoice}! Nobody wins!`;
-        }
-    }
-
-    // Update display div
-    scoreReport.textContent = `Your Score: ${humanScore} | Computer Score: ${computerScore}`;
-}
